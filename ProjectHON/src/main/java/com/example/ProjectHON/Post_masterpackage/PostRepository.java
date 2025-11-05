a372de98668dfc3701ad7a7ec5a07c656ae43aba
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-11-05 21:03:39 +0530

    Update PostRepository.java

--- ProjectHON/src/main/java/com/example/ProjectHON/Post_masterpackage/PostRepository.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/Post_masterpackage/PostRepository.java
@@ -1,8 +1,17 @@
 package com.example.ProjectHON.Post_masterpackage;
 
+import com.example.ProjectHON.User_masterpackage.UserMaster;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<PostMaster,Long> {
+    List<PostMaster> findByPostIdNotIn(List<Integer> postIds);
 
+//    @Query("SELECT pm from PostMaster pm where pm.user=:user")
+    List<PostMaster> findByUser(UserMaster user);
 }
 
