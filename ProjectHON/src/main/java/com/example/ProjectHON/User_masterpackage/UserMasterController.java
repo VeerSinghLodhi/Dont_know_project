ac05f310203bdbbf8e6599573740409ced85d82e
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-11-06 11:16:17 +0530

    Update UserMasterController.java

--- ProjectHON/src/main/java/com/example/ProjectHON/User_masterpackage/UserMasterController.java
+++ ProjectHON/src/main/java/com/example/ProjectHON/User_masterpackage/UserMasterController.java
@@ -28,65 +28,62 @@ public class UserMasterController {
     @Autowired
     RatingRepository ratingRepository;
 
-
-    @PostMapping("/login")
-    public String getPostLogin(@RequestParam("email")String email,
-                               @RequestParam("password")String password,
-                               Model model,
-                               HttpSession session){
-
-        UserMaster userMaster=userMasterRepository.findByEmailAndPassword(email,password);
-        if(userMaster!=null){
-            session.setAttribute("user_master",userMaster);
-            System.out.println("Redirect Successful!!");
-            return "redirect:/user/dashboard";
-        }
-        System.out.println("Invalid Credential");
-        model.addAttribute("error",true);
-        return "login";
+    @GetMapping("/")
+    public String getLogin(){
+        return "redirect:/login";
     }
 
-    @GetMapping("/user/dashboard")
-    public String getUserDashboard(Model model,HttpSession session){
-        UserMaster userMaster = (UserMaster) session.getAttribute("user_master");
-
-        if (userMaster == null) {
-            return "redirect:/login"; // if session expired
-        }
-
-        // 1️⃣ Get all post IDs already rated by this user
-        List<Integer> ratedPostIds = ratingRepository.findRatedPostIdsByUser(userMaster.getUserId());
-
-        // 2️⃣ Fetch posts that are NOT rated yet
-        List<PostMaster> unRatedPosts;
-
-        if (ratedPostIds.isEmpty()) {
-            unRatedPosts = postRepository.findAll(); // if no ratings yet
-        } else {
-            unRatedPosts = postRepository.findByPostIdNotIn(ratedPostIds);
-        }
-
-        List<PostMaster> postMasterList=postRepository.findByUser(userMaster);
-
-        Double sumAllPostRating=0.0;
-        int count=0;
-        for(PostMaster postMaster : postMasterList){
-            Double rating=0.0;
-            for(RatingMaster ratingMaster : postMaster.getRatings()) {
-                rating += ratingMaster.getRating();
-                count++;
-            }
-            sumAllPostRating +=rating;
-        }
-
-        // 3️⃣ Pass data to frontend
-        model.addAttribute("posts", unRatedPosts);
-        model.addAttribute("allPostRatingAverage",String.format("%.2f", sumAllPostRating/count));
-        model.addAttribute("sumAllPostRating",String.format("%.2f", sumAllPostRating));
-        model.addAttribute("own_user_posts", postMasterList);
-        model.addAttribute("user_master", userMaster);
-        return "rating";
+    @GetMapping("/signup")
+    public String getSignUp(){
+        return "signup";
     }
+
+
+
+
+
+//    @GetMapping("/user/dashboard")
+//    public String getUserDashboard(Model model,HttpSession session){
+//        UserMaster userMaster = (UserMaster) session.getAttribute("user_master");
+//
+//        System.out.println("Inside the User Dashboard Method" +userMaster.getEmail());
+//        if (userMaster == null) {
+//            return "redirect:/login"; // if session expired
+//        }
+//
+//        // 1️⃣ Get all post IDs already rated by this user
+//        List<Integer> ratedPostIds = ratingRepository.findRatedPostIdsByUser(userMaster.getUserId());
+//
+//        // 2️⃣ Fetch posts that are NOT rated yet
+//        List<PostMaster> unRatedPosts;
+//
+//        if (ratedPostIds.isEmpty()) {
+//            unRatedPosts = postRepository.findAll(); // if no ratings yet
+//        } else {
+//            unRatedPosts = postRepository.findByPostIdNotIn(ratedPostIds);
+//        }
+//
+//        List<PostMaster> postMasterList=postRepository.findByUser(userMaster);
+//
+//        Double sumAllPostRating=0.0;
+//        int count=0;
+//        for(PostMaster postMaster : postMasterList){
+//            Double rating=0.0;
+//            for(RatingMaster ratingMaster : postMaster.getRatings()) {
+//                rating += ratingMaster.getRating();
+//                count++;
+//            }
+//            sumAllPostRating +=rating;
+//        }
+//
+//        // 3️⃣ Pass data to frontend
+//        model.addAttribute("posts", unRatedPosts);
+//        model.addAttribute("allPostRatingAverage",String.format("%.2f", sumAllPostRating/count));
+//        model.addAttribute("sumAllPostRating",String.format("%.2f", sumAllPostRating));
+//        model.addAttribute("own_user_posts", postMasterList);
+//        model.addAttribute("user_master", userMaster);
+//        return "rating";
+//    }
 
     @GetMapping("/userphoto/{postId}")
     ResponseEntity<byte[]> getUserProfile(@PathVariable("postId")Long postId){
